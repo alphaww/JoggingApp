7a2e739c983c61755a4ac9fca1157eabc6f5bd5c
Alem <alem@liquidx.com>
2022-10-12 10:28:19 +0200

    Registered domain events interceptor to ef core

--- api/JoggingApp/Setup/StorageSetup.cs
+++ api/JoggingApp/Setup/StorageSetup.cs
@@ -2,6 +2,7 @@
 using JoggingApp.Core.Jogs;
 using JoggingApp.Core.Users;
 using JoggingApp.EntityFramework;
+using JoggingApp.EntityFramework.Interceptors;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,7 +13,10 @@ namespace JoggingApp.Setup
     {
         public static void AddStorage(this WebApplicationBuilder builder)
         {
-            builder.Services.AddDbContext<JoggingAppDbContext>(context => context.UseSqlServer(builder.Configuration["ConnectionString:DefaultConnection"]));
+            builder.Services.AddDbContext<JoggingAppDbContext>(context =>
+                context.UseSqlServer(builder.Configuration["ConnectionString:DefaultConnection"])
+                    .AddInterceptors(new DomainEventsToOutboxInterceptor()));
+
             builder.Services.AddScoped<IUserStorage, UserStorage>();
             builder.Services.AddScoped<IJogStorage, JogStorage>();
         }
