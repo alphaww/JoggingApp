25c907611ddbc0980736e8fa3cb015eec5fe0ad1
Alem <alem@liquidx.com>
2022-10-14 09:26:05 +0200

    Added concrete IEventBus implementation ( RabbitMQ )

---
+++ api/ClassLibrary1/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿global using Microsoft.Extensions.Logging;
+global using Polly;
+global using Polly.Retry;
+global using RabbitMQ.Client;
+global using RabbitMQ.Client.Events;
+global using RabbitMQ.Client.Exceptions;
+global using System;
+global using System.IO;
+global using System.Net.Sockets;
+global using JoggingApp.BuildingBlocks.EventBus;
+global using JoggingApp.BuildingBlocks.EventBus.Abstractions;
+global using JoggingApp.BuildingBlocks.EventBus.Events;
+global using JoggingApp.BuildingBlocks.EventBus.Extensions;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Text.Json;
