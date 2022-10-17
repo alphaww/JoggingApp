4ff09a2ebdcb52ca594e4cc016c88333ea4c3157
Alem <alem@liquidx.com>
2022-10-17 21:12:29 +0200

    service sync

---
+++ api/JoggingApp.OutboxPublishingAgent/BackgroundPublishingServiceSync.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JoggingApp.Core;
+using JoggingApp.Core.Outbox;
+using MediatR;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
+using Quartz;
+
+namespace JoggingApp.OutboxPublishingAgent
+{
+    [DisallowConcurrentExecution]
+    public class BackgroundPublishingService : IJob
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new()
+        {
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        private readonly IPublisher _publisher;
+        private readonly IOutboxStorage _outboxStorage;
+        public BackgroundPublishingService(IPublisher publisher, IOutboxStorage outboxStorage)
+        {
+            _publisher = publisher;
+            _outboxStorage = outboxStorage;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var outboxEvents = await _outboxStorage.GetOutboxEventsAsync();
+
+            foreach (OutboxMessage @event in outboxEvents)
+            {
+                IDomainEvent domainEvent = JsonConvert.DeserializeObject<IDomainEvent>(@event.Content, JsonSerializerSettings);
+
+                if (domainEvent is null)
+                {
+                    continue;
+                }
+
+                AsyncRetryPolicy policy = Policy.Handle<Exception>().WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(50 * attempt));
+
+                PolicyResult result = await policy.ExecuteAndCaptureAsync(() => _publisher.Publish(domainEvent, context.CancellationToken));
+
+
+                @event.SetEventState(result.Outcome == OutcomeType.Successful ? OutboxMessageState.Done: OutboxMessageState.Fail);
+                await _outboxStorage.UpdateOutboxEventAsync(@event);
+            }
+        }
+    }
+}
