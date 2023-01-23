169265462ba449f34c3c16386887f03b9717371e
Alem <alem@liquidx.com>
2023-01-23 21:51:25 +0100

    bla

--- api/JoggingApp.OutboxPublishingAgent/OutboxStorage.cs
+++ api/JoggingApp.OutboxPublishingAgent/OutboxStorage.cs
@@ -1,8 +1,6 @@
-﻿using JoggingApp.Core.Outbox;
-using SqlKata.Execution;
+﻿using Dapper;
+using JoggingApp.Core.Outbox;
 using System.Data;
-using System.Data.Common;
-using Dapper;
 
 namespace JoggingApp.OutboxPublishingAgent
 {
@@ -15,36 +13,31 @@ namespace JoggingApp.OutboxPublishingAgent
             _connection = connection;
         }
 
-
         public async Task<IEnumerable<OutboxMessage>> MarkAndGetOutboxEvents()
         {
             var now = DateTime.UtcNow;
-            var sql = @"UPDATE outbox SET ProcessStartedAt = @now, EventState = @pendingState 
-                WHERE EventState = @readyState AND ProcessStartedAt IS NULL 
-                AND OccurredOnUtc <= @now 
-                OUTPUT inserted.*";
+            var sql = @"UPDATE OutboxMessage SET EventState = @pendingState
+                        OUTPUT deleted.*
+                        WHERE EventState = @readyState AND ProcessedOnUtc IS NULL AND OccurredOnUtc <= @now";
 
-            using var transaction = _connection.BeginTransaction();
             var outboxEvents = await _connection.QueryAsync<OutboxMessage>(sql,
-                new { now, pendingState = OutboxMessageState.Pending, readyState = OutboxMessageState.Ready },
-                transaction);
-
-            transaction.Commit();
+                new { now, pendingState = OutboxMessageState.Pending, readyState = OutboxMessageState.Ready });
 
             return outboxEvents.ToList();
         }
 
         public async Task UpdateOutboxEventAsync(OutboxMessage outboxEvent)
         {
-            var sql = @"UPDATE outbox SET EventState = @EventState, ProcessedOnUtc = @ProcessedOnUtc, Error = @Error 
+            var sql = @"UPDATE OutboxMessage SET EventState = @EventState, ProcessedOnUtc = @ProcessedOnUtc, Error = @Error 
                 WHERE Id = @Id";
 
             await _connection.ExecuteAsync(sql, outboxEvent);
+
         }
 
         public async Task InsertOutboxEventAsync(OutboxMessage outboxEvent)
         {
-            var sql = @"INSERT INTO outbox (Id, Type, Content, EventType, EventState, OccurredOnUtc, ProcessedOnUtc, Error)
+            var sql = @"INSERT INTO OutboxMessage (Id, Type, Content, EventType, EventState, OccurredOnUtc, ProcessedOnUtc, Error)
                 VALUES (@Id, @Type, @Content, @EventType, @EventState, @OccurredOnUtc, @ProcessedOnUtc, @Error)";
 
             await _connection.ExecuteAsync(sql, outboxEvent);
